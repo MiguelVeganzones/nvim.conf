f8b18d4210489f98aee6b5b69f04d21ad53677b0
Miguel Veganzones <miguel.veganzones@intel.com>
2025-12-01 22:03:39 +0100

    Improve .gitignore support for oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,49 +1,70 @@
+-- helper function to parse output
+local function parse_output(proc)
+    local result = proc:wait()
+    local ret = {}
+    if result.code == 0 then
+        for line in vim.gsplit(result.stdout, "\n", { plain = true, trimempty = true }) do
+            -- Remove trailing slash
+            line = line:gsub("/$", "")
+            ret[line] = true
+        end
+    end
+    return ret
+end
+
+-- build git status cache
+local function new_git_status()
+    return setmetatable({}, {
+        __index = function(self, key)
+            local ignore_proc = vim.system(
+                { "git", "ls-files", "--ignored", "--exclude-standard", "--others", "--directory" },
+                {
+                    cwd = key,
+                    text = true,
+                }
+            )
+            local tracked_proc = vim.system({ "git", "ls-tree", "HEAD", "--name-only" }, {
+                cwd = key,
+                text = true,
+            })
+            local ret = {
+                ignored = parse_output(ignore_proc),
+                tracked = parse_output(tracked_proc),
+            }
+
+            rawset(self, key, ret)
+            return ret
+        end,
+    })
+end
+local git_status = new_git_status()
+
+-- Clear git status cache on refresh
+local refresh = require("oil.actions").refresh
+local orig_refresh = refresh.callback
+refresh.callback = function(...)
+    git_status = new_git_status()
+    orig_refresh(...)
+end
+
 require("oil").setup({
     default_file_explorer = true,
     columns = { "icon" },
     view_options = {
-        show_hidden = false,
         is_hidden_file = function(name, bufnr)
-            if vim.endswith(name, ".") then
-                return false
+            local dir = require("oil").get_current_dir(bufnr)
+            local is_dotfile = vim.startswith(name, ".") and name ~= ".."
+            -- if no local directory (e.g. for ssh connections), just hide dotfiles
+            if not dir then
+                return is_dotfile
             end
-
-            if vim.endswith(name, "errors.log") or vim.endswith(name, "general.log") then
-                return false
+            -- dotfiles are considered hidden unless tracked
+            if is_dotfile then
+                return not git_status[dir].tracked[name]
+            else
+                -- Check if file is gitignored
+                return git_status[dir].ignored[name]
             end
-
-            -- Get the full path of the current buffer
-            local bufname = vim.api.nvim_buf_get_name(bufnr)
-            -- Convert the oil:// format to a regular file path
-            local filepath = bufname:gsub("^oil://", "") -- Remove the oil:// prefix
-
-            -- Get the directory of the file
-            local dirpath = vim.fn.fnamemodify(filepath, ":h") -- Get the directory path
-
-            -- Check if the directory is a git repository
-            local is_git_repo_cmd = "git -C " .. dirpath .. " rev-parse --is-inside-work-tree 2>/dev/null"
-            local git_repo_handle = io.popen(is_git_repo_cmd)
-            local is_git_repo_result = git_repo_handle and git_repo_handle:read("*a") or ""
-            if git_repo_handle then
-                git_repo_handle:close()
-            end
-
-            -- If not in a git repository, check for dotfiles
-            if is_git_repo_result == "" then
-                -- Check for dotfiles only if not in a git repo
-                return vim.startswith(name, ".")
-            end
-
-            -- If we are in a git repository, run git check-ignore on the specific file to see if it's gitignored
-            local git_check_cmd = "git -C " .. dirpath .. " check-ignore " .. vim.fn.shellescape(name)
-            local git_handle = io.popen(git_check_cmd)
-            local git_result = git_handle and git_handle:read("*a") or ""
-            if git_handle then
-                git_handle:close()
-            end
-
-            -- Hide the file if it is either git-ignored or a dotfile
-            return (git_result ~= "") or vim.startswith(name, ".")
         end,
         is_always_hidden = function(name, bufnr)
             return false
--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,4 +1,10 @@
-vim.g.vimtex_view_method = 'zathura'
+if vim.fn.executable('zathura') == 1 then
+    vim.g.vimtex_view_method = 'zathura'
+else
+    vim.g.vimtex_view_method = 'general'
+    vim.g.vimtex_view_general_viewer = 'SumatraPDF'
+    vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
+end
 vim.g.vimtex_compiler_method = 'latexmk'
 vim.g.maplocalleader = " "
 vim.g.vimtex_quickfix_mode = 0
