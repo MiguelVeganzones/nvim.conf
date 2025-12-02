496e44aa67ea9c3da3b3bbad5fc27caf0fb7857b
Miguel Veganzones <miguel.veganzones@intel.com>
2025-12-02 20:28:26 +0100

    Add vimtex submodule support

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -5,6 +5,8 @@ else
     vim.g.vimtex_view_general_viewer = 'SumatraPDF'
     vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
 end
+vim.g.vimtex_root_patterns = {'.latexmkrc', 'main.tex', '*.tex'}
 vim.g.vimtex_compiler_method = 'latexmk'
 vim.g.maplocalleader = " "
 vim.g.vimtex_quickfix_mode = 0
+vim.g.vimtex_subfile = 1
